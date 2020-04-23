a1e361fbceb182986fa56614304066d72b4cee1e
Matthew Martin <zhintateo19924559@gmail.com>
2020-04-22 20:31:51 -0500

    Got the Props to take the movieList in

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -13,6 +15,7 @@ const App = () => {
         .get('http://localhost:5000/api/movies')
         .then(response => {
           setMovieList(response.data);
+          console.log(response.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -28,7 +31,13 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Router>
+        <Switch>
+          <Route path="/">
+            <MovieList movies={movieList} />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
